c39d2e44ea47a84e57628c475425fb6e353a1c92
YAGO LUIZ DA SILVA <yago.lsilva2@senacsp.edu.br>
2026-04-06 17:06:46 -0300

    EnvioCadastroProduto

---
+++ WindowsFormsApp1/CadastrarProduto.Designer.cs
@@ -0,0 +1,238 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class CadastrarProduto
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblNomeProd = new System.Windows.Forms.Label();
+            this.txtNomeProd = new System.Windows.Forms.TextBox();
+            this.txtDescricaoProd = new System.Windows.Forms.TextBox();
+            this.lblDescricaoProd = new System.Windows.Forms.Label();
+            this.btnCadastraProd = new System.Windows.Forms.Button();
+            this.lblValorProd = new System.Windows.Forms.Label();
+            this.lblCategoriaProd = new System.Windows.Forms.Label();
+            this.cmbCategoriaProd = new System.Windows.Forms.ComboBox();
+            this.txtValorProd = new System.Windows.Forms.TextBox();
+            this.txtMarcaProd = new System.Windows.Forms.TextBox();
+            this.lblMarcaProd = new System.Windows.Forms.Label();
+            this.txtAlturaProd = new System.Windows.Forms.TextBox();
+            this.lblAlturaProd = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.lblLarguraProd = new System.Windows.Forms.Label();
+            this.txtPesoProd = new System.Windows.Forms.TextBox();
+            this.lblPesoProd = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblNomeProd
+            // 
+            this.lblNomeProd.AutoSize = true;
+            this.lblNomeProd.Location = new System.Drawing.Point(12, 9);
+            this.lblNomeProd.Name = "lblNomeProd";
+            this.lblNomeProd.Size = new System.Drawing.Size(129, 13);
+            this.lblNomeProd.TabIndex = 1;
+            this.lblNomeProd.Text = "Digite o Nome do Produto";
+            // 
+            // txtNomeProd
+            // 
+            this.txtNomeProd.Location = new System.Drawing.Point(13, 26);
+            this.txtNomeProd.Name = "txtNomeProd";
+            this.txtNomeProd.Size = new System.Drawing.Size(201, 20);
+            this.txtNomeProd.TabIndex = 2;
+            // 
+            // txtDescricaoProd
+            // 
+            this.txtDescricaoProd.Location = new System.Drawing.Point(13, 66);
+            this.txtDescricaoProd.Name = "txtDescricaoProd";
+            this.txtDescricaoProd.Size = new System.Drawing.Size(429, 20);
+            this.txtDescricaoProd.TabIndex = 4;
+            // 
+            // lblDescricaoProd
+            // 
+            this.lblDescricaoProd.AutoSize = true;
+            this.lblDescricaoProd.Location = new System.Drawing.Point(12, 49);
+            this.lblDescricaoProd.Name = "lblDescricaoProd";
+            this.lblDescricaoProd.Size = new System.Drawing.Size(149, 13);
+            this.lblDescricaoProd.TabIndex = 3;
+            this.lblDescricaoProd.Text = "Digite a Descrição do Produto";
+            // 
+            // btnCadastraProd
+            // 
+            this.btnCadastraProd.Location = new System.Drawing.Point(13, 328);
+            this.btnCadastraProd.Name = "btnCadastraProd";
+            this.btnCadastraProd.Size = new System.Drawing.Size(124, 60);
+            this.btnCadastraProd.TabIndex = 5;
+            this.btnCadastraProd.Text = "Cadastrar Produto";
+            this.btnCadastraProd.UseVisualStyleBackColor = true;
+            // 
+            // lblValorProd
+            // 
+            this.lblValorProd.AutoSize = true;
+            this.lblValorProd.Location = new System.Drawing.Point(12, 169);
+            this.lblValorProd.Name = "lblValorProd";
+            this.lblValorProd.Size = new System.Drawing.Size(86, 13);
+            this.lblValorProd.TabIndex = 6;
+            this.lblValorProd.Text = "Valor do Produto";
+            // 
+            // lblCategoriaProd
+            // 
+            this.lblCategoriaProd.AutoSize = true;
+            this.lblCategoriaProd.Location = new System.Drawing.Point(12, 128);
+            this.lblCategoriaProd.Name = "lblCategoriaProd";
+            this.lblCategoriaProd.Size = new System.Drawing.Size(107, 13);
+            this.lblCategoriaProd.TabIndex = 7;
+            this.lblCategoriaProd.Text = "Categoria do Produto";
+            // 
+            // cmbCategoriaProd
+            // 
+            this.cmbCategoriaProd.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbCategoriaProd.FormattingEnabled = true;
+            this.cmbCategoriaProd.Location = new System.Drawing.Point(15, 145);
+            this.cmbCategoriaProd.Name = "cmbCategoriaProd";
+            this.cmbCategoriaProd.Size = new System.Drawing.Size(149, 21);
+            this.cmbCategoriaProd.TabIndex = 8;
+            // 
+            // txtValorProd
+            // 
+            this.txtValorProd.Location = new System.Drawing.Point(15, 185);
+            this.txtValorProd.Name = "txtValorProd";
+            this.txtValorProd.Size = new System.Drawing.Size(83, 20);
+            this.txtValorProd.TabIndex = 9;
+            // 
+            // txtMarcaProd
+            // 
+            this.txtMarcaProd.Location = new System.Drawing.Point(15, 105);
+            this.txtMarcaProd.Name = "txtMarcaProd";
+            this.txtMarcaProd.Size = new System.Drawing.Size(83, 20);
+            this.txtMarcaProd.TabIndex = 11;
+            // 
+            // lblMarcaProd
+            // 
+            this.lblMarcaProd.AutoSize = true;
+            this.lblMarcaProd.Location = new System.Drawing.Point(12, 89);
+            this.lblMarcaProd.Name = "lblMarcaProd";
+            this.lblMarcaProd.Size = new System.Drawing.Size(92, 13);
+            this.lblMarcaProd.TabIndex = 10;
+            this.lblMarcaProd.Text = "Marca do Produto";
+            // 
+            // txtAlturaProd
+            // 
+            this.txtAlturaProd.Location = new System.Drawing.Point(15, 224);
+            this.txtAlturaProd.Name = "txtAlturaProd";
+            this.txtAlturaProd.Size = new System.Drawing.Size(83, 20);
+            this.txtAlturaProd.TabIndex = 13;
+            // 
+            // lblAlturaProd
+            // 
+            this.lblAlturaProd.AutoSize = true;
+            this.lblAlturaProd.Location = new System.Drawing.Point(12, 208);
+            this.lblAlturaProd.Name = "lblAlturaProd";
+            this.lblAlturaProd.Size = new System.Drawing.Size(125, 13);
+            this.lblAlturaProd.TabIndex = 12;
+            this.lblAlturaProd.Text = "Altura do Produto em CM";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(15, 263);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(83, 20);
+            this.textBox1.TabIndex = 15;
+            // 
+            // lblLarguraProd
+            // 
+            this.lblLarguraProd.AutoSize = true;
+            this.lblLarguraProd.Location = new System.Drawing.Point(12, 247);
+            this.lblLarguraProd.Name = "lblLarguraProd";
+            this.lblLarguraProd.Size = new System.Drawing.Size(134, 13);
+            this.lblLarguraProd.TabIndex = 14;
+            this.lblLarguraProd.Text = "Largura do Produto em CM";
+            // 
+            // txtPesoProd
+            // 
+            this.txtPesoProd.Location = new System.Drawing.Point(15, 302);
+            this.txtPesoProd.Name = "txtPesoProd";
+            this.txtPesoProd.Size = new System.Drawing.Size(83, 20);
+            this.txtPesoProd.TabIndex = 17;
+            // 
+            // lblPesoProd
+            // 
+            this.lblPesoProd.AutoSize = true;
+            this.lblPesoProd.Location = new System.Drawing.Point(12, 286);
+            this.lblPesoProd.Name = "lblPesoProd";
+            this.lblPesoProd.Size = new System.Drawing.Size(121, 13);
+            this.lblPesoProd.TabIndex = 16;
+            this.lblPesoProd.Text = "Peso do Produto em KG";
+            // 
+            // CadastrarProduto
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtPesoProd);
+            this.Controls.Add(this.lblPesoProd);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.lblLarguraProd);
+            this.Controls.Add(this.txtAlturaProd);
+            this.Controls.Add(this.lblAlturaProd);
+            this.Controls.Add(this.txtMarcaProd);
+            this.Controls.Add(this.lblMarcaProd);
+            this.Controls.Add(this.txtValorProd);
+            this.Controls.Add(this.cmbCategoriaProd);
+            this.Controls.Add(this.lblCategoriaProd);
+            this.Controls.Add(this.lblValorProd);
+            this.Controls.Add(this.btnCadastraProd);
+            this.Controls.Add(this.txtDescricaoProd);
+            this.Controls.Add(this.lblDescricaoProd);
+            this.Controls.Add(this.txtNomeProd);
+            this.Controls.Add(this.lblNomeProd);
+            this.Name = "CadastrarProduto";
+            this.Text = "CadastrarProduto";
+            this.Load += new System.EventHandler(this.CadastrarProduto_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label lblNomeProd;
+        private System.Windows.Forms.TextBox txtNomeProd;
+        private System.Windows.Forms.TextBox txtDescricaoProd;
+        private System.Windows.Forms.Label lblDescricaoProd;
+        private System.Windows.Forms.Button btnCadastraProd;
+        private System.Windows.Forms.Label lblValorProd;
+        private System.Windows.Forms.Label lblCategoriaProd;
+        private System.Windows.Forms.ComboBox cmbCategoriaProd;
+        private System.Windows.Forms.TextBox txtValorProd;
+        private System.Windows.Forms.TextBox txtMarcaProd;
+        private System.Windows.Forms.Label lblMarcaProd;
+        private System.Windows.Forms.TextBox txtAlturaProd;
+        private System.Windows.Forms.Label lblAlturaProd;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label lblLarguraProd;
+        private System.Windows.Forms.TextBox txtPesoProd;
+        private System.Windows.Forms.Label lblPesoProd;
+    }
+}
