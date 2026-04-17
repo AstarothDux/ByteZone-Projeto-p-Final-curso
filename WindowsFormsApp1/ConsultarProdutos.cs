d7a764d144dab98e4dcfae4a65f790afece1a8a0
AstarothDux <yagoluiz2005@gmail.com>
2026-04-17 00:02:48 -0300

    brunch up teste

---
+++ WindowsFormsApp1/ConsultarProdutos.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    class ConsultarProdutos
+    {
+        SqlConnection conn = new SqlConnection(Variaveis.strConn);
+        public void consultarProdutos(string consulta)
+        {
+            try
+            {
+                conn.Open();
+                //Criar comando de consulta
+                SqlCommand comando = new SqlCommand(consulta, conn);
+                //Criar dataReader
+                SqlDataReader drDados = null;
+                //Fazer a consulta
+                drDados = comando.ExecuteReader();
+                while (drDados.Read())
+                {
+                    //Obter resultados das colunas
+                    string NomeProduto = (string)drDados["Nome_Produto"];
+                    //Preencher combobox com dados
+                    Variaveis.CaixaTxtNomeProd = NomeProduto;
+                }
+            }
+            catch (SqlException s)
+            {
+                MessageBox.Show(s.Source.ToString());
+            }
+        }
+    }
+}
